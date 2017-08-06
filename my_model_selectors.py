effc7b043929ac25625910700c754e6bdefe20fb
Vedpathak <omkarv@amazon.com>
2017-08-05 20:20:26 -0700

    Initial submission of project - implemented ASL recognizer and selectors

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -7,7 +7,6 @@ from hmmlearn.hmm import GaussianHMM
 from sklearn.model_selection import KFold
 from asl_utils import combine_sequences
 
-
 class ModelSelector(object):
     '''
     base class for model selection (strategy design pattern)
@@ -32,7 +31,6 @@ class ModelSelector(object):
         raise NotImplementedError
 
     def base_model(self, num_states):
-        # with warnings.catch_warnings():
         warnings.filterwarnings("ignore", category=DeprecationWarning)
         # warnings.filterwarnings("ignore", category=RuntimeWarning)
         try:
@@ -46,6 +44,56 @@ class ModelSelector(object):
                 print("failure on {} with {} states".format(self.this_word, num_states))
             return None
 
+    def base_selector(self, custom_scoring_method):
+        warnings.filterwarnings("ignore", category=DeprecationWarning)
+        split_method = KFold()
+        best_score_sum = -math.inf
+        best_hmm_model = None
+        for num_states in range(self.min_n_components, self.max_n_components):
+            hmm_model_score_sum = 0
+            if len(self.sequences) is 1:
+                return GaussianHMM(n_components=num_states, covariance_type="diag", n_iter=1000,
+                                        random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
+            elif len(self.sequences) is 2:
+                normalized_X, normalized_lengths = combine_sequences([0], self.sequences)
+                test_X, test_lengths = combine_sequences([1], self.sequences)
+                try:
+                    hmm_model = GaussianHMM(n_components=num_states, covariance_type="diag", n_iter=1000,
+                                            random_state=self.random_state, verbose=False).fit(normalized_X, normalized_lengths)
+                    hmm_model_score_sum += self.custom_scoring_method(hmm_model, test_X, test_lengths, num_states)
+                    if self.verbose:
+                        print("model created for {} with {} states".format(self.this_word, num_states))
+                        print("model score is {}".format(hmm_model_score_sum))
+                except:
+                    if self.verbose:
+                        print("failure on {} with {} states".format(self.this_word, num_states))
+            else:
+                for cv_train_idx, cv_test_idx in split_method.split(self.sequences):
+                    normalized_X, normalized_lengths = combine_sequences(cv_train_idx, self.sequences)
+                    test_X, test_lengths = combine_sequences(cv_test_idx, self.sequences)
+                    try:
+                        hmm_model = GaussianHMM(n_components=num_states, covariance_type="diag", n_iter=1000,
+                                                random_state=self.random_state, verbose=False).fit(normalized_X, normalized_lengths)
+                        hmm_model_score_sum += self.custom_scoring_method(hmm_model, test_X, test_lengths, num_states)
+                        if self.verbose:
+                            print("model created for {} with {} states".format(self.this_word, num_states))
+                            print("model score is {}".format(hmm_model_score_sum))
+                    except:
+                        if self.verbose:
+                            print("failure on {} with {} states".format(self.this_word, num_states))
+            if best_hmm_model is None:
+                best_hmm_model = hmm_model
+            if hmm_model_score_sum > best_score_sum:
+                best_score_sum = hmm_model_score_sum
+                best_n = num_states
+                best_hmm_model = hmm_model
+        self.print_best_score()
+        return best_hmm_model
+
+    def print_best_score(self):
+        if self.verbose:
+            print("best score is {}".format(best_score_sum))
+            print("best n is {}".format(best_n))
 
 class SelectorConstant(ModelSelector):
     """ select the model with value self.n_constant
@@ -68,6 +116,9 @@ class SelectorBIC(ModelSelector):
     Bayesian information criteria: BIC = -2 * logL + p * logN
     """
 
+    def custom_scoring_method(self, hmm_model, test_X, test_lengths, num_params):
+        return -2 * hmm_model.score(test_X, test_lengths) + len(test_X) * math.log10(num_params)
+
     def select(self):
         """ select the best model for self.this_word based on
         BIC score for n between self.min_n_components and self.max_n_components
@@ -76,8 +127,7 @@ class SelectorBIC(ModelSelector):
         """
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        return self.base_selector(self.custom_scoring_method)
 
 
 class SelectorDIC(ModelSelector):
@@ -88,21 +138,26 @@ class SelectorDIC(ModelSelector):
     http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.58.6208&rep=rep1&type=pdf
     DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
     '''
+    def custom_scoring_method(self, hmm_model, test_X, test_lengths, num_params):
+        antiLogL = 0
+        for word in self.hwords:
+            if word == self.this_word:
+                continue
+            X, lengths = self.hwords[word]
+            antiLogL += hmm_model.score(X, lengths)
+        return hmm_model.score(test_X, test_lengths) - 1 / ( len(self.words) * antiLogL )
 
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-        # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        return self.base_selector(self.custom_scoring_method)
 
 
 class SelectorCV(ModelSelector):
     ''' select best model based on average log Likelihood of cross-validation folds
 
     '''
+    def custom_scoring_method(self, hmm_model, test_X, test_lengths, num_params):
+        return hmm_model.score(test_X, test_lengths)
 
     def select(self):
-        warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-        # TODO implement model selection using CV
-        raise NotImplementedError
+        return self.base_selector(self.custom_scoring_method)
--- my_recognizer.py
+++ my_recognizer.py
@@ -1,6 +1,6 @@
 import warnings
+import math
 from asl_data import SinglesData
-
 
 def recognize(models: dict, test_set: SinglesData):
     """ Recognize test word sequences from word models set
@@ -20,6 +20,24 @@ def recognize(models: dict, test_set: SinglesData):
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     probabilities = []
     guesses = []
-    # TODO implement the recognizer
-    # return probabilities, guesses
-    raise NotImplementedError
+    input_word_list = test_set.get_all_sequences()
+    hwords = test_set.get_all_Xlengths()
+
+    for word_id in range(len(input_word_list)):
+        best_score = -math.inf
+        best_word = ""
+        X, lengths = hwords[word_id]
+        probability_dict = {}
+        for word in models:
+            model = models[word]
+            try:
+                score = model.score(X, lengths)
+                if score > best_score:
+                    best_score = score
+                    best_word = word
+                probability_dict[word] = score
+            except:
+                print("failure to score word: {} using model".format(word))
+        probabilities.append(probability_dict)
+        guesses.append(best_word)
+    return (probabilities, guesses)
